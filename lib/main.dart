3a909faebe26b93b09b0f4ddf008ce7b0518e8dc
h0996g <h0996g@gmail.com>
2024-03-18 01:19:11 +0100

    marge with main wmb3d rad main

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,16 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:pfeprojet/component/const.dart';
 import 'package:pfeprojet/design_login.dart';
+import 'package:pfeprojet/design_register.dart';
 import 'package:pfeprojet/firebase_options.dart';
 import 'package:pfeprojet/helper/cachhelper.dart';
 import 'package:pfeprojet/helper/observer.dart';
 import 'package:pfeprojet/screen/AdminScreens/home/cubit/home_admin_cubit.dart';
 import 'package:pfeprojet/screen/AdminScreens/home/home.dart';
 import 'package:pfeprojet/screen/AdminScreens/profile/cubit/profile_admin_cubit.dart';
-import 'package:pfeprojet/screen/AdminScreens/terrains/cubit/terrain_cubit.dart';
+import 'package:pfeprojet/screen/Auth/Login.dart';
 import 'package:pfeprojet/screen/Auth/cubit/auth_cubit.dart';
+import 'package:pfeprojet/screen/Auth/onboarding.dart';
 import 'package:pfeprojet/screen/joueurScreens/home/home.dart';
 import 'package:pfeprojet/screen/joueurScreens/profile/cubit/profile_cubit.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -22,10 +24,10 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  Widget startWidget = LoginDesign();
+  Widget startWidget = Login();
   // CachHelper.removdata(key: "TOKEN");
+  bool onbordingmain = await CachHelper.getData(key: 'onbording') ?? false;
   TOKEN = await CachHelper.getData(key: 'TOKEN') ?? '';
-
   if (TOKEN != '') {
     DECODEDTOKEN = JwtDecoder.decode(TOKEN);
     if (DECODEDTOKEN['role'] == 'joueur') {
@@ -37,12 +39,15 @@ void main() async {
 
   runApp(MyApp(
     startwidget: startWidget,
+    onbordingmain: onbordingmain,
   ));
 }
 
 class MyApp extends StatelessWidget {
   final Widget startwidget;
-  const MyApp({super.key, required this.startwidget});
+  final bool onbordingmain;
+  const MyApp(
+      {super.key, required this.startwidget, required this.onbordingmain});
 
   // This widget is the root of your application.
   @override
@@ -56,9 +61,6 @@ class MyApp extends StatelessWidget {
           create: ((context) => HomeAdminCubit()..getMyInfo()),
         ),
         BlocProvider(
-          create: ((context) => TerrainCubit()),
-        ),
-        BlocProvider(
           create: ((context) => ProfileAdminCubit()),
         ),
         BlocProvider(
@@ -67,8 +69,83 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: startwidget,
+        home: const Onbording(),
+        // home: onbordingmain ? startwidget : const Onbording(),
       ),
     );
   }
 }
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:jwt_decoder/jwt_decoder.dart';
+// import 'package:pfeprojet/component/const.dart';
+// import 'package:pfeprojet/design_login.dart';
+// import 'package:pfeprojet/firebase_options.dart';
+// import 'package:pfeprojet/helper/cachhelper.dart';
+// import 'package:pfeprojet/helper/observer.dart';
+// import 'package:pfeprojet/screen/AdminScreens/home/cubit/home_admin_cubit.dart';
+// import 'package:pfeprojet/screen/AdminScreens/home/home.dart';
+// import 'package:pfeprojet/screen/AdminScreens/profile/cubit/profile_admin_cubit.dart';
+// import 'package:pfeprojet/screen/AdminScreens/terrains/cubit/terrain_cubit.dart';
+// import 'package:pfeprojet/screen/Auth/cubit/auth_cubit.dart';
+// import 'package:pfeprojet/screen/joueurScreens/home/home.dart';
+// import 'package:pfeprojet/screen/joueurScreens/profile/cubit/profile_cubit.dart';
+// import 'package:firebase_core/firebase_core.dart';
+
+// void main() async {
+//   Bloc.observer = MyBlocObserver();
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await CachHelper.init();
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+//   Widget startWidget = LoginDesign();
+//   // CachHelper.removdata(key: "TOKEN");
+//   TOKEN = await CachHelper.getData(key: 'TOKEN') ?? '';
+
+//   if (TOKEN != '') {
+//     DECODEDTOKEN = JwtDecoder.decode(TOKEN);
+//     if (DECODEDTOKEN['role'] == 'joueur') {
+//       startWidget = const HomeJoueur();
+//     } else if (DECODEDTOKEN['role'] == 'admin') {
+//       startWidget = const HomeAdmin();
+//     }
+//   }
+
+//   runApp(MyApp(
+//     startwidget: startWidget,
+//   ));
+// }
+
+// class MyApp extends StatelessWidget {
+//   final Widget startwidget;
+//   const MyApp({super.key, required this.startwidget});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MultiBlocProvider(
+//       providers: [
+//         BlocProvider(
+//           create: ((context) => AuthCubit()),
+//         ),
+//         BlocProvider(
+//           create: ((context) => HomeAdminCubit()..getMyInfo()),
+//         ),
+//         BlocProvider(
+//           create: ((context) => TerrainCubit()),
+//         ),
+//         BlocProvider(
+//           create: ((context) => ProfileAdminCubit()),
+//         ),
+//         BlocProvider(
+//           create: ((context) => ProfileCubit()..getMyInfo()),
+//         ),
+//       ],
+//       child: MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         home: startwidget,
+//       ),
+//     );
+//   }
+// }
