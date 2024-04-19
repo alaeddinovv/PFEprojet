c449518bb805fec060962fc1ef5c78ae5f0ccf71
h0996g <h0996g@gmail.com>
2024-04-19 05:47:04 +0100

    Add AddTimeBlockDialog component and update update_terrain.dart to use it

--- lib/screen/AdminScreens/terrains/add_terrain.dart
+++ lib/screen/AdminScreens/terrains/add_terrain.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pfeprojet/Model/terrain_model.dart';
+import 'package:pfeprojet/component/add_time_block.dart';
 import 'package:pfeprojet/component/components.dart';
 import 'package:pfeprojet/component/const.dart';
 import 'package:pfeprojet/screen/AdminScreens/terrains/location/add_location_terrain.dart';
@@ -269,9 +270,15 @@ class _AddTerrainPageState extends State<AddTerrainPage> {
                                       text: block.hours!.join(', ')),
                                   // Update hours on change
                                   onChanged: (newHours) {
+                                    // block.hours = newHours
+                                    //     .split(',')
+                                    //     .map((e) => e.trim())
+                                    //     .toList();
                                     block.hours = newHours
                                         .split(',')
-                                        .map((e) => e.trim())
+                                        .map(
+                                            (e) => normalizeTimeInput(e.trim()))
+                                        .where((hour) => hour != "Invalid Time")
                                         .toList();
                                   },
                                 ),
@@ -525,70 +532,5 @@ void _showDeleteConfirmation(
   );
   if (result ?? false) {
     cubit.removeNonReservableTimeBlock(index);
-  }
-}
-
-class AddTimeBlockDialog extends StatefulWidget {
-  const AddTimeBlockDialog({Key? key}) : super(key: key);
-
-  @override
-  _AddTimeBlockDialogState createState() => _AddTimeBlockDialogState();
-}
-
-class _AddTimeBlockDialogState extends State<AddTimeBlockDialog> {
-  String? selectedDay;
-  List<String> hours = [];
-
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
-      title: const Text('Add Non-Reservable Time Block'),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          DropdownButton<String>(
-            hint: const Text('Select Day'),
-            value: selectedDay,
-            onChanged: (value) => setState(() => selectedDay = value),
-            items: [
-              'Monday',
-              'Tuesday',
-              'Wednesday',
-              'Thursday',
-              'Friday',
-              'Saturday',
-              'Sunday'
-            ]
-                .map((day) => DropdownMenuItem(value: day, child: Text(day)))
-                .toList(),
-          ),
-          TextField(
-            onSubmitted: (value) {},
-            decoration:
-                const InputDecoration(hintText: 'Hours (e.g., 8, 9:30, 8.30)'),
-            onChanged: (value) {
-              hours = value
-                  .split(',')
-                  .map((e) => normalizeTimeInput(e.trim()))
-                  .where((hour) => hour != "Invalid Time")
-                  .toList();
-            },
-          ),
-        ],
-      ),
-      actions: [
-        TextButton(
-            onPressed: () => Navigator.of(context).pop(),
-            child: const Text('Cancel')),
-        TextButton(
-            onPressed: () {
-              if (selectedDay != null && hours.isNotEmpty) {
-                Navigator.of(context).pop(
-                    NonReservableTimeBlocks(day: selectedDay!, hours: hours));
-              }
-            },
-            child: const Text('Add')),
-      ],
-    );
   }
 }
--- lib/screen/AdminScreens/terrains/update_terrain.dart
+++ lib/screen/AdminScreens/terrains/update_terrain.dart
@@ -1,19 +1,17 @@
 import 'dart:io';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:pfeprojet/Model/terrain_model.dart';
+import 'package:pfeprojet/component/add_time_block.dart';
 import 'package:pfeprojet/component/components.dart';
 import 'package:pfeprojet/component/const.dart';
 import 'package:pfeprojet/screen/AdminScreens/terrains/cubit/terrain_cubit.dart';
 import 'package:pfeprojet/screen/AdminScreens/terrains/location/add_location_terrain.dart';
 
 class EditTerrainPage extends StatefulWidget {
-  // Assuming AnnonceModel is your data model
   final TerrainModel terrainModel;
 
   const EditTerrainPage({Key? key, required this.terrainModel})
@@ -559,70 +557,5 @@ class _EditTerrainPageState extends State<EditTerrainPage> {
         );
       }
     });
-  }
-}
-
-class AddTimeBlockDialog extends StatefulWidget {
-  const AddTimeBlockDialog({Key? key}) : super(key: key);
-
-  @override
-  _AddTimeBlockDialogState createState() => _AddTimeBlockDialogState();
-}
-
-class _AddTimeBlockDialogState extends State<AddTimeBlockDialog> {
-  String? selectedDay;
-  List<String> hours = [];
-
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
-      title: const Text('Add Non-Reservable Time Block'),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          DropdownButton<String>(
-            hint: const Text('Select Day'),
-            value: selectedDay,
-            onChanged: (value) => setState(() => selectedDay = value),
-            items: [
-              'Monday',
-              'Tuesday',
-              'Wednesday',
-              'Thursday',
-              'Friday',
-              'Saturday',
-              'Sunday'
-            ]
-                .map((day) => DropdownMenuItem(value: day, child: Text(day)))
-                .toList(),
-          ),
-          TextField(
-            onSubmitted: (value) {},
-            decoration:
-                const InputDecoration(hintText: 'Hours (e.g., 8, 9:30, 8.30)'),
-            onChanged: (value) {
-              hours = value
-                  .split(',')
-                  .map((e) => normalizeTimeInput(e.trim()))
-                  .where((hour) => hour != "Invalid Time")
-                  .toList();
-            },
-          ),
-        ],
-      ),
-      actions: [
-        TextButton(
-            onPressed: () => Navigator.of(context).pop(),
-            child: const Text('Cancel')),
-        TextButton(
-            onPressed: () {
-              if (selectedDay != null && hours.isNotEmpty) {
-                Navigator.of(context).pop(
-                    NonReservableTimeBlocks(day: selectedDay!, hours: hours));
-              }
-            },
-            child: const Text('Add')),
-      ],
-    );
   }
 }
